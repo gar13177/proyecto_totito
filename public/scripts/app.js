f6b08fc8866bac5912b78e6401ec698cda992b1e
Kevin García <gar13177@uvg.edu.gt>
2016-07-31 16:16:51 -0600

    entrega para revision
    ya funciona el totito. falta revisar el uso del tag 'a' en html, pero
    parece que hace bien el trabajo de recargar la pagina. Faltaria guardar
    el puntaje de los jugadores

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,26 +3,48 @@ var actual_turn = 1;
 var positions_logic = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
 function is_full(){
-  var value = true;
+  var full = true;
   positions_logic.forEach(function(array){
     array.forEach(function(element){
       if (element === 0){
-        value = false;
+        full = false;
       }
     });
   });
-  return value;
+  return full;
 }
 
-function check_winner() {
-  //cambiar orden porque primero revisaria si ya no hay mas posiciones
-  //esto quiere decir que si en el ultimo turno se gana, daria error
-  //ya que no detectaria que con ese turno gano
-  if (!is_full()) {
-    console.log("Nuevo Turno");
-  }else{
+function check_winner(j,i) {
+  var winner = 0;//0 indica que todavia no han ganado
+  var count = positions_logic[j].filter(function(x) {return x === positions_logic[j][i]}).length;
+  if (count === 3){
+    winner = positions_logic[j][i];
+    console.log("ganador: "+winner);
+  }
+
+  if (positions_logic[0][i] === positions_logic[1][i] && positions_logic[1][i] === positions_logic[2][i]){
+    winner = positions_logic[j][i];
+    console.log("ganador: "+winner);
+  }
+
+  if (['00','11','22'].indexOf(''+j+''+i) > -1){
+    if (positions_logic[0][0] === positions_logic[1][1] && positions_logic[1][1] === positions_logic[2][2]){
+      winner = positions_logic[j][i];
+      console.log("ganador: "+winner);
+    }
+  }else if (['02','11','20'].indexOf(''+j+''+i) > -1){
+    if (positions_logic[0][2] === positions_logic[1][1] && positions_logic[1][1] === positions_logic[2][0]){
+      winner = positions_logic[j][i];
+      console.log("ganador: "+winner);
+    }
+  }
+
+  if (winner === 0 && is_full()){
+    winner = -1;//No hay mas turnos
     console.log("No hay mas turnos");
   }
+
+  return winner;
 }
 
 function new_click() {
@@ -41,7 +63,43 @@ function new_click() {
       this.style.backgroundColor = '#ecaf4f';
       actual_turn = 1;
     }
-    check_winner();
+    var winner = check_winner(this_j-1,this_i-1);
+    if ( winner != 0){//si existe ganandor o ya no hay turnos
+        var player = document.getElementById("player");
+        if (winner === -1){
+          player.innerHTML = "Es un empate";
+        }else{
+          player.innerHTML = "El ganador es el jugador "+winner;
+        }
+        var end = document.getElementById('0_0');
+        end.style.display = 'inline';
+    }
+  }
+}
+
+function on_mouse_over(){
+  var id = this.id;
+  var this_j = id.split('_')[0];
+  var this_i = id.split('_')[1];
+  if (positions_logic[this_j - 1][this_i - 1] === 0) {
+    if (actual_turn === 1){
+      this.innerHTML = 'X';
+    }else{
+      this.innerHTML = 'O';
+    }
+  }
+}
+
+function on_mouse_out(){
+  var id = this.id;
+  var this_j = id.split('_')[0];
+  var this_i = id.split('_')[1];
+  if (positions_logic[this_j - 1][this_i - 1] === 0) {
+    if (actual_turn === 1){
+      this.innerHTML = '';
+    }else{
+      this.innerHTML = '';
+    }
   }
 }
 
@@ -49,147 +107,7 @@ for (var j = 1; j <= positions_logic.length; j++) {
   for (var i = 1; i <= positions_logic[j - 1].length; i++) {
     var box = document.getElementById('' + j + '_' + i);
     box.onclick = new_click;
+    box.onmouseover = on_mouse_over;
+    box.onmouseout = on_mouse_out;
   }
 }
-
-/*
-var reloj = "1";
-var selector = document.getElementById("selector");
-var deg_h = 0;
-var deg_m = 0;
-var deg_s = 0;
-
-function start(){
-  document.getElementById("selector").addEventListener("change", addActivityItem, false);
-}
-
-function addActivityItem(){
-  reloj = selector.options[selector.selectedIndex].value;
-  change_clock();
-  render();
-}
-
-function changeTime(){
-  deg_s = deg_s + 6;
-  if (deg_s >= 360){
-    deg_s = deg_s - 360;
-    deg_m = deg_m + 6;
-  }
-  if (deg_m >= 360){
-    deg_m = deg_m - 360;
-    deg_h = deg_h + 30;
-  }
-  if (deg_h >= 360){
-    deg_h = deg_h - 360;
-  }
-  render();  
-}
-
-function render(){
-  if (reloj === "1"){
-    var seg = document.getElementById("_segundero");
-    seg.style.transform = "rotate(" + deg_s + "deg)";
-
-    var min = document.getElementById("_minutero");
-    min.style.transform = "rotate(" + deg_m + "deg)";
-
-    var hor = document.getElementById("_horero");
-    hor.style.transform = "rotate(" + deg_h + "deg)";
-    
-  }else{
-    var hora = deg_h / 30;
-    var min = deg_m / 6;
-    var seg = deg_s / 6;
-
-    var text = ""+hora+" : "+min+" : "+seg;
-    document.getElementById("_digital").innerHTML = text;
-  }
-}
-
-
-function change_clock(){
-  var html = "";
-  if (reloj === "1"){
-    html = '<div class="reloj">';
-    for (i = 1; i <= 12; i++) { 
-      html += '<div class="numero n'+i+'">'+i+'</div>';
-    }
-    
-    html += '<div id="_horero" class="aguja horero"></div>';
-    html += '<div id="_minutero" class="aguja minutero"></div>';
-    html += '<div id="_segundero" class="aguja segundero"></div>';
-    html += '</div>';
-  }else{
-    html = '<div id="_digital" class="digital"></div>'
-  }
-  var viewport = document.getElementById("container");
-  viewport.innerHTML = html;
-}
-
-change_clock();
-window.addEventListener("load", start, false);
-window.setInterval(changeTime, 1000);
-*/
-
-
-//scss
-/*$bottom_pos: 0px;
-$left_pos: 255px;
-
-.reloj{
-  width: 500px;
-  height: 500px;
-  border-radius: 50%;
-  background: gray;
-  border: 5px solid black;
-  margin-left: 0;
-  margin-top: 0;
-}
-
-
-@mixin numero(){
-  height: 250px;
-  color: black;
-  font-size: 50px;
-  line-height: 80px;
-  position: absolute;
-  margin-left: $left_pos;
-  top: $bottom_pos;
-  transform-origin: bottom left;
-  @for $i from 1 through (12){
-    &.n#{$i}{
-      transform: rotate($i * 30deg);
-    }
-  }
-}
-
-.numero{
-  @include numero();
-}
-
-.aguja{
-  height: 230px;
-  background: black;
-  transform-origin: bottom center;
-  position: absolute;
-  left: $left_pos;
-  top: $bottom_pos + 20px;
-}
-
-.horero{
-  width: 10px;
-}
-
-.minutero{
-  width: 5px;
-}
-
-.segundero{
-  width: 3px;
-  background: red;
-}
-
-.digital{
-  font-size: 100px;
-}
-*/
