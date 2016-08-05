d1095e3b134462464b6290a68cb6b4b0935aeab2
Kevin García <gar13177@uvg.edu.gt>
2016-08-04 23:33:30 -0600

    proyecto listo para entrega
    se quito el cambio de propiedades del css y se cambio por html puro. el
    hover se volvio una clase y el manejo de la logica contiene ahora un
    estado de clic y un estado de mouse sobre el cuadro y fuera del cuadro

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,11 @@
 
-var actual_turn = 1;
-var positions_logic = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+var state = {};
+state.actual_turn = 1;
+state.positions_logic = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
 function is_full(){
   var full = true;
-  positions_logic.forEach(function(array){
+  state.positions_logic.forEach(function(array){
     array.forEach(function(element){
       if (element === 0){
         full = false;
@@ -16,27 +17,27 @@ function is_full(){
 
 function check_winner(j,i) {
   var winner = 0;//0 indica que todavia no han ganado
-  var count = positions_logic[j].filter(function(x) {return x === positions_logic[j][i]}).length;
+  var count = state.positions_logic[j].filter(function(x) {return x === state.positions_logic[j][i]}).length;
   if (count === 3){
-    winner = positions_logic[j][i];
+    winner = state.positions_logic[j][i];
     //console.log("ganador: "+winner);
   }
 
-  if (positions_logic[0][i] === positions_logic[1][i] && positions_logic[1][i] === positions_logic[2][i]){
-    winner = positions_logic[j][i];
+  if (state.positions_logic[0][i] === state.positions_logic[1][i] && state.positions_logic[1][i] === state.positions_logic[2][i]){
+    winner = state.positions_logic[j][i];
     //console.log("ganador: "+winner);
   }
 
   if (['00','11','22'].indexOf(''+j+''+i) > -1){
-    if (positions_logic[0][0] === positions_logic[1][1] && positions_logic[1][1] === positions_logic[2][2]){
-      winner = positions_logic[j][i];
+    if (state.positions_logic[0][0] === state.positions_logic[1][1] && state.positions_logic[1][1] === state.positions_logic[2][2]){
+      winner = state.positions_logic[j][i];
       //console.log("ganador: "+winner);
     }
   }
   
   if (['02','11','20'].indexOf(''+j+''+i) > -1){
-    if (positions_logic[0][2] === positions_logic[1][1] && positions_logic[1][1] === positions_logic[2][0]){
-      winner = positions_logic[j][i];
+    if (state.positions_logic[0][2] === state.positions_logic[1][1] && state.positions_logic[1][1] === state.positions_logic[2][0]){
+      winner = state.positions_logic[j][i];
       //console.log("ganador: "+winner);
     }
   }
@@ -53,28 +54,13 @@ function new_click() {
   var id = this.id;
   var this_j = id.split('_')[0];
   var this_i = id.split('_')[1];
-  if (positions_logic[this_j - 1][this_i - 1] === 0) {
-    positions_logic[this_j - 1][this_i - 1] = actual_turn;
-    
-    if (actual_turn === 1) {
-      this.innerHTML = 'X';
-      this.style.backgroundColor = '#dc685a';
-      actual_turn = 2;
-    } else {
-      this.innerHTML = 'O';
-      this.style.backgroundColor = '#ecaf4f';
-      actual_turn = 1;
-    }
-    var winner = check_winner(this_j-1,this_i-1);
-    if ( winner != 0){//si existe ganandor o ya no hay turnos
-        var player = document.getElementById("player");
-        if (winner === -1){
-          player.innerHTML = "Es un empate";
-        }else{
-          player.innerHTML = "El ganador es el jugador "+winner;
-        }
-        var end = document.getElementById('0_0');
-        end.style.display = 'inline';
+  if (state.positions_logic[this_j - 1][this_i - 1] <= 0) {
+    state.positions_logic[this_j - 1][this_i - 1] = state.actual_turn;
+    render(build_html(state)+build_html_winner(this_j,this_i));
+    if (state.actual_turn === 1){
+      state.actual_turn = 2;
+    }else{
+      state.actual_turn = 1;
     }
   }
 }
@@ -83,12 +69,9 @@ function on_mouse_over(){
   var id = this.id;
   var this_j = id.split('_')[0];
   var this_i = id.split('_')[1];
-  if (positions_logic[this_j - 1][this_i - 1] === 0) {
-    if (actual_turn === 1){
-      this.innerHTML = 'X';
-    }else{
-      this.innerHTML = 'O';
-    }
+  if (state.positions_logic[this_j - 1][this_i - 1] == 0) {
+    state.positions_logic[this_j - 1][this_i - 1] = -state.actual_turn;//solo se coloca 
+    render(build_html(state));
   }
 }
 
@@ -96,38 +79,76 @@ function on_mouse_out(){
   var id = this.id;
   var this_j = id.split('_')[0];
   var this_i = id.split('_')[1];
-  if (positions_logic[this_j - 1][this_i - 1] === 0) {
-    this.innerHTML = '';
+  if (state.positions_logic[this_j - 1][this_i - 1] < 0) {
+    state.positions_logic[this_j - 1][this_i - 1] = 0;
+    render(build_html(state));
   }
 }
 
+function build_html_winner(this_j, this_i){
+  var winner = check_winner(this_j-1,this_i-1);
+  var html = '';
+  if ( winner != 0){
+      if (winner === -1){
+        html += '<div class="end" id="0_0">';
+        html += '<h1 id="player">Es un empate</h1>';
+      }else{
+        
+        html += '<div class="end" id="0_0">';
+        html += '<h1 id="player">El ganador es el jugador '+winner+'</h1>';
+      }
+      html += '<a href="">Nuevo Juego</a>';
+  }else{
+    html += '<div class="end hide" id="0_0">';
+  }
+  html += '</div>';
+  return html;
+}
 
-
-function first_render(){
+function build_html(state){
   var html = "";
-  for (var j = 1; j <= positions_logic.length; j++){
-    for (var i = 1; i <= positions_logic[j - 1].length; i++){
-      html += '<div class="box b'+j+i+'" id="'+j+'_'+i+'"></div>';
+  for (var j = 1; j <= state.positions_logic.length; j++){
+    for (var i = 1; i <= state.positions_logic[j - 1].length; i++){
+      html += '<div id="'+j+'_'+i+'" class="box b'+j+''+i ;
+      switch (state.positions_logic[j-1][i-1]){
+        case 1:
+          html += ' clic_x">X</div>';
+          break;
+        case -1:
+          html += ' hover">X</div>';
+          break;
+        case 2:
+          html += ' clic_o">O</div>';
+          break;
+        case -2:
+          html += ' hover">O</div>';
+          break;
+        default:
+          html += '"></div>';
+          break;
+      }
     }
   }
+  return html;
+}
 
-  html += '<div class="end" id="0_0">';
-  html += '<h1 id="player"></h1>';
-  html += '<a href="">Nuevo Juego</a>';
-  html += '</div>';
 
+function render(html){
   var viewport = document.getElementById("all");
   viewport.innerHTML = html;
-
+  set_actions();
 }
 
-first_render();
-
-for (var j = 1; j <= positions_logic.length; j++) {
-  for (var i = 1; i <= positions_logic[j - 1].length; i++) {
-    var box = document.getElementById('' + j + '_' + i);
-    box.onclick = new_click;
-    box.onmouseover = on_mouse_over;
-    box.onmouseout = on_mouse_out;
+function set_actions(){
+  for (var j = 1; j <= state.positions_logic.length; j++) {
+    for (var i = 1; i <= state.positions_logic[j - 1].length; i++) {
+      var box = document.getElementById('' + j + '_' + i);
+      box.onclick = new_click;
+      box.onmouseover = on_mouse_over;
+      box.onmouseout = on_mouse_out;
+    }
   }
 }
+
+render(build_html(state));
+set_actions();
